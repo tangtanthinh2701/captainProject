769c87d38f02d18525f186cee049d5c44cef3909
Tan <tangtanthinh2701@gmail.com>
2025-02-18 15:15:05 +0700

    First version

---
+++ src/main/java/com/captainProject/marketingProject/MarketingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.captainProject.marketingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingProjectApplication.class, args);
+	}
+
+}
